f1885032f34a99681ab7fec763be36e975c76653
Andres <barathum196@gmail.com>
2018-04-13 11:24:03 -0500

    Proyecto iniciado...Interfaces creadas(sin terminar)

---
+++ app/src/main/java/amartine59/tallerlv_sc/Triangulo.java
@@ -0,0 +1,13 @@
+package amartine59.tallerlv_sc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Triangulo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_triangulo);
+    }
+}
