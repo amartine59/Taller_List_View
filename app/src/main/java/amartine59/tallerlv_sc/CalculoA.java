de43a6b2a1d4256279165b85b0d85bfbd68a6cdf
Andres <barathum196@gmail.com>
2018-04-16 00:08:02 -0500

    Calculo de areas terminado

--- app/src/main/java/amartine59/tallerlv_sc/CalculoA.java
+++ app/src/main/java/amartine59/tallerlv_sc/CalculoA.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class CalculoA extends AppCompatActivity {
     private LinearLayout area_cuadrado;
@@ -43,7 +44,20 @@ public class CalculoA extends AppCompatActivity {
         area_triangulo.setVisibility(LinearLayout.GONE);
         area_circulo.setVisibility(LinearLayout.GONE);
 
-
+        switch (operacion_a_realizar){
+            case "cuadrado":
+                area_cuadrado.setVisibility(LinearLayout.VISIBLE);
+                break;
+            case "rectangulo":
+                area_rectangulo.setVisibility(LinearLayout.VISIBLE);
+                break;
+            case "triangulo":
+                area_triangulo.setVisibility(LinearLayout.VISIBLE);
+                break;
+            case "circulo":
+                area_circulo.setVisibility(LinearLayout.VISIBLE);
+                break;
+        }
 
     }
     public void almacenarOperacion(String operacion,String []datosOperacion,String resultado){
@@ -52,7 +66,7 @@ public class CalculoA extends AppCompatActivity {
 
     }
 
-    public void limpiar(View v){
+    public void limpiar(){
         lado_cuadrado.setText("");
         base_rectangulo.setText("");
         altura_rectangulo.setText("");
@@ -72,5 +86,61 @@ public class CalculoA extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    public void calcular_area_cuadrado(View v){
+        if (validar(lado_cuadrado)){
+            Double valor_lado = Double.parseDouble(lado_cuadrado.getText().toString());
+            Double resultado = valor_lado * valor_lado;
+            String lado = getResources().getString(R.string.lado)+"= "+valor_lado;
+            String [] datos_operacion = {lado};
+
+            almacenarOperacion(getResources().getString(R.string.area_cuadrado),datos_operacion,resultado+"");
+            Toast.makeText(this,getResources().getString(R.string.resultado)+"\n"+(resultado+""),Toast.LENGTH_SHORT);
+            limpiar();
+        }
+    }
+
+    public void calcular_area_rectangulo(View v){
+        if (validar(base_rectangulo)&& validar(altura_rectangulo)){
+            Double valor_base = Double.parseDouble(base_rectangulo.getText().toString());
+            Double valor_altura = Double.parseDouble(altura_rectangulo.getText().toString());
+            Double resultado = valor_base * valor_altura;
+            String base_altura = getResources().getString(R.string.base)+"= "+valor_base+"\n"+
+                                 getResources().getString(R.string.altura)+"= "+valor_altura;
+            String [] datos_operacion = {base_altura};
+
+            almacenarOperacion(getResources().getString(R.string.area_rectangulo),datos_operacion,resultado+"");
+            Toast.makeText(this,getResources().getString(R.string.resultado)+"\n"+(resultado+""),Toast.LENGTH_SHORT);
+            limpiar();
+        }
+    }
+
+    public void calcular_area_triangulo(View v){
+        if (validar(base_triangulo)&& validar(altura_triangulo)){
+            Double valor_base = Double.parseDouble(base_triangulo.getText().toString());
+            Double valor_altura = Double.parseDouble(altura_triangulo.getText().toString());
+            Double resultado = (valor_base * valor_altura)/2;
+            String base_altura = getResources().getString(R.string.base)+"= "+valor_base+"\n"+
+                    getResources().getString(R.string.altura)+"= "+valor_altura;
+            String [] datos_operacion = {base_altura};
+
+            almacenarOperacion(getResources().getString(R.string.area_triangulo),datos_operacion,resultado+"");
+            Toast.makeText(this,getResources().getString(R.string.resultado)+"\n"+(resultado+""),Toast.LENGTH_SHORT);
+            limpiar();
+        }
+    }
+
+    public void calcular_area_circulo(View v){
+        if (validar(radio_circulo)){
+            Double valor_radio = Double.parseDouble(lado_cuadrado.getText().toString());
+            Double resultado = valor_radio * (3.141592653589793*3.141592653589793);
+            String radio = getResources().getString(R.string.radio)+"= "+valor_radio;
+            String [] datos_operacion = {radio};
+
+            almacenarOperacion(getResources().getString(R.string.area_circulo),datos_operacion,resultado+"");
+            Toast.makeText(this,getResources().getString(R.string.resultado)+"\n"+(resultado+""),Toast.LENGTH_SHORT);
+            limpiar();
+        }
     }
 }
